6032659546dc26f17b0b23476fce314ffd180df8
guptasonu14 <sguptabudha@gmail.com>
2024-02-15 11:51:13 +0530

    recorrect folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
- import {Home} from './_root/pages';
- import SigninForm from './_auth/forms/SigninForm';
- import SignupForm from './_auth/forms/SignupForm';
-  import AuthLayout from './_auth/AuthLayout';
-  import RootLayout from './_root/RootLayout';
- import './globals.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import { Home } from './_root/pages';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
-<Route element={<AuthLayout/>}> 
-      
-      <Route path="/sign-in" element={SigninForm/>}/>
-      <Route path="/sign-up" element={SignupForm/>}/>
-      <Route index element={<Home/>}/>
-      </Routes> 
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-up" element={<SignupForm />} />
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<RootLayout />} />
+          <Route path="/home" element={<Home />} />
+        </Route>
+      </Routes>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
