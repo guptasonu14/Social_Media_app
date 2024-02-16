27208ef72edc5b91d4b134109b0266ec74d7e68b
Rishabh Rai <rishabh8403@gmail.com>
2024-02-16 10:10:26 +0530

    Auth pages configuration

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,16 @@ import RootLayout from './_root/RootLayout';
 const App = () => {
   return (
     <main className="flex h-screen">
-      <Routes>
-        <Route path="/sign-in" element={<SigninForm />} />
-        <Route path="/sign-up" element={<SignupForm />} />
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<RootLayout />} />
-          <Route path="/home" element={<Home />} />
+     <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </main>
