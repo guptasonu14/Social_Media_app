fe1d8fbe028bd1c48d12b50a51a5df8ecd3b8cb0
guptasonu14 <sguptabudha@gmail.com>
2024-02-21 23:31:48 +0530

    add validation in email

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -6,7 +6,16 @@ import * as z from "zod";
 export const SignupValidation = z.object({
   name: z.string().min(2, { message: "Name must be at least 2 characters." }),
   username: z.string().min(2, { message: "Name must be at least 2 characters." }),
-  email: z.string().email(),
+  email: z.string().refine((value) => {
+    // Define the allowed domain
+    const allowedDomain = 'lpu.in';
+
+    // Define the required format using a regular expression
+    const emailFormat = /^[0-9]{8}\.[a-zA-Z]+@lpu\.in$/;
+
+    // Check if the email matches the required format and domain
+    return emailFormat.test(value) && value.endsWith(allowedDomain);
+  }, { message: "Invalid email address or domain." }),
   password: z.string().min(8, { message: "Password must be at least 8 characters." }),
 });
 
@@ -14,7 +23,6 @@ export const SigninValidation = z.object({
   email: z.string().email(),
   password: z.string().min(8, { message: "Password must be at least 8 characters." }),
 });
-
 export const ProfileValidation = z.object({
   file: z.custom<File[]>(),
   name: z.string().min(2, { message: "Name must be at least 2 characters." }),
